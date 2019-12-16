d52c3aa3d62af3df8402ba30901cea90cff184d4
Mike DLoss <mike.dloss@gmail.com>
2019-12-15 21:32:36 -0500

    Add GTM

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,13 +1,14 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const { buildRSSPluginOptions } = require('./scripts/gatsby-config');
+const { buildRSSPluginOptions } = require("./scripts/gatsby-config");
 
 module.exports = {
   siteMetadata: {
     title: `Harvestland Church`,
     author: `Mike DLoss`,
-    siteUrl: 'https://harvestland.church',
-    description: "Harvestland Church exists to win people to Jesus, build a community of believers, and to equip those people to do God's work."
+    siteUrl: "https://harvestland.church",
+    description:
+      "Harvestland Church exists to win people to Jesus, build a community of believers, and to equip those people to do God's work."
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -15,12 +16,12 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `assets`,
-        path: `${__dirname}/src/assets`,
-      },
+        path: `${__dirname}/src/assets`
+      }
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    'gatsby-transformer-remark',
+    "gatsby-transformer-remark",
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
@@ -30,11 +31,11 @@ module.exports = {
         background_color: `#fff`,
         theme_color: `#42613d`,
         display: `minimal-ui`,
-        icon: `src/assets/images/favicons/favicon1024.png`,
-      },
+        icon: `src/assets/images/favicons/favicon1024.png`
+      }
     },
     {
-      resolve: 'gatsby-plugin-react-svg',
+      resolve: "gatsby-plugin-react-svg",
       options: {
         rule: {
           include: /assets/
@@ -42,18 +43,35 @@ module.exports = {
       }
     },
     {
-      resolve: 'gatsby-source-contentful',
+      resolve: "gatsby-source-contentful",
       options: {
-        spaceId: `${ process.env.CONTENTFUL_SPACE_ID }`,
-        accessToken: `${ process.env.CONTENTFUL_DELIVERY_TOKEN }`,
+        spaceId: `${process.env.CONTENTFUL_SPACE_ID}`,
+        accessToken: `${process.env.CONTENTFUL_DELIVERY_TOKEN}`
       }
     },
-    '@contentful/gatsby-transformer-contentful-richtext',
-    'gatsby-plugin-sitemap',
-    'gatsby-plugin-robots-txt',
-    process.env.USE_RSS_PLUGIN ? buildRSSPluginOptions() : 'Did you add USE_RSS_PLUGIN to the .env file?',
+    "@contentful/gatsby-transformer-contentful-richtext",
+    "gatsby-plugin-sitemap",
+    "gatsby-plugin-robots-txt",
+    {
+      resolve: "gatsby-plugin-google-tagmanager",
+      options: {
+        id: "GTM-WJ3964L",
+
+        // Include GTM in development.
+        // Defaults to false meaning GTM will only be loaded in production.
+        includeInDevelopment: false,
+
+        // datalayer to be set before GTM is loaded
+        // should be an object or a function that is executed in the browser
+        // Defaults to null
+        defaultDataLayer: { platform: "gatsby" }
+      }
+    },
+    process.env.USE_RSS_PLUGIN
+      ? buildRSSPluginOptions()
+      : "Did you add USE_RSS_PLUGIN to the .env file?"
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
     // 'gatsby-plugin-offline',
-  ],
-}
+  ]
+};
