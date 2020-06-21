707e8bd05d0280ba3a6a53856374f2272a6c5eca
Mike DLoss <mike.dloss@gmail.com>
2020-06-20 21:56:03 -0400

    change link to live videos

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -11,38 +11,38 @@ import * as Styled from "./Layout.styles";
 import { BannerAlert } from "../BannerAlert/BannerAlert";
 
 export const Layout = ({ children, fullWidth, hideLogo }) => {
-  const headerProps = { hideLogo };
+    const headerProps = { hideLogo };
 
-  return (
-    <ThemeProvider theme={theme}>
-      <Styled.SiteContainer isFullWidth={fullWidth}>
-        <GlobalStyle />
-        <Header {...headerProps} />
-        <BannerAlert>
-          <BannerAlert.ExternalLink
-            href="https://www.facebook.com/HarvestlandOpenBible"
-            rel="noopener noreferrer"
-            target="_blank"
-          >
-            <BannerAlert.Text>
-              Online Services on Facebook - Click Here
-            </BannerAlert.Text>
-          </BannerAlert.ExternalLink>
-        </BannerAlert>
-        <main>{children}</main>
-        <Footer />
-      </Styled.SiteContainer>
-    </ThemeProvider>
-  );
+    return (
+        <ThemeProvider theme={theme}>
+            <Styled.SiteContainer isFullWidth={fullWidth}>
+                <GlobalStyle />
+                <Header {...headerProps} />
+                <BannerAlert>
+                    <BannerAlert.ExternalLink
+                        href="https://www.facebook.com/HarvestlandOpenBible/live_videos/"
+                        rel="noopener noreferrer"
+                        target="_blank"
+                    >
+                        <BannerAlert.Text>
+                            Online Services on Facebook - Click Here
+                        </BannerAlert.Text>
+                    </BannerAlert.ExternalLink>
+                </BannerAlert>
+                <main>{children}</main>
+                <Footer />
+            </Styled.SiteContainer>
+        </ThemeProvider>
+    );
 };
 
 Layout.propTypes = {
-  fullWidth: PropTypes.bool,
-  hideLogo: PropTypes.bool,
-  children: PropTypes.node.isRequired
+    fullWidth: PropTypes.bool,
+    hideLogo: PropTypes.bool,
+    children: PropTypes.node.isRequired,
 };
 
 Layout.defaultProps = {
-  fullWidth: false,
-  hideLogo: false
+    fullWidth: false,
+    hideLogo: false,
 };
