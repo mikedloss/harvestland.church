a09110d978ef3ee7ffeaefd02fa43bb626f45dd0
Mike DLoss <mike.dloss@gmail.com>
2020-07-16 16:20:21 -0400

    update leaders

--- src/pages/about/leaders.js
+++ src/pages/about/leaders.js
@@ -1,20 +1,20 @@
-import React from "react";
-import { graphql } from "gatsby";
-import { Heading, Flex } from "rebass";
+import React from 'react';
+import { graphql } from 'gatsby';
+import { Heading, Flex } from 'rebass';
 
-import { Layout, SEO, Hero, LeaderInfo, Container } from "../../components";
+import { Layout, SEO, Hero, LeaderInfo, Container } from '../../components';
 
 const LeadersPage = ({ data: { heroImage, ...leaders } }) => {
-  console.log("leaders :", leaders);
+  console.log('leaders :', leaders);
   return (
     <Layout>
       <SEO
         title="Leaders"
-        keywords={["harvestland church leaders", "leaders"]}
+        keywords={['harvestland church leaders', 'leaders']}
       />
       <Hero.Image
         imageSrc={heroImage.childImageSharp.fluid.src}
-        height={["40vh", "50vh"]}
+        height={['40vh', '50vh']}
       >
         <Heading p="20px" fontSize={[5, 6]}>
           Our Leaders
@@ -22,7 +22,7 @@ const LeadersPage = ({ data: { heroImage, ...leaders } }) => {
       </Hero.Image>
       <Container>
         <Flex flexDirection="column" alignItems="center">
-          {Object.keys(leaders).map(leader => (
+          {Object.keys(leaders).map((leader) => (
             <LeaderInfo who={leaders[leader]} key={leader.name} />
           ))}
         </Flex>
@@ -60,9 +60,6 @@ export const query = graphql`
       ...LeaderData
     }
     willis: contentfulLeader(name: { eq: "Willis Greer" }) {
-      ...LeaderData
-    }
-    summer: contentfulLeader(name: { eq: "Summer Posey" }) {
       ...LeaderData
     }
   }
